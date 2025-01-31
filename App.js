2d1573ee1f7bf87c9f218b5801b69fbc9d2570f4
monika pant <monikapant@mukeshs-MacBook-Pro.local>
2025-01-31 23:19:32 +0530

    episode-1: practical

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
         // const heading = React.createElement('h1', {id:"heading1", xyz:"testing"}, 'my react element');
         // const root = ReactDOM.createRoot(document.getElementById("root"));
         // root.render(heading);   
+        import React from "react";
+        import ReactDOM from "react-dom";
 
-const parent = React.createElement('div', {id:"parent"}, [
-        React.createElement('div', {id:"sibling1"}, [
-                React.createElement('h1', {id:"heading1"}, 'child1 h1'),
-                React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
-        ]),
-        React.createElement('div', {id:"sibling2"}, [
-                React.createElement('h1', {id:"heading1"}, 'child1 h1'),
-                React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
-        ])
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);   
+        const parent = React.createElement('div', {id:"parent"}, [
+                React.createElement('div', {id:"sibling1"}, [
+                        React.createElement('h1', {id:"heading1"}, 'child1 h1'),
+                        React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
+                ]),
+                React.createElement('div', {id:"sibling2"}, [
+                        React.createElement('h1', {id:"heading1"}, 'child1 h1'),
+                        React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
+                ])
+        ]);
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+                root.render(parent);   
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Namaste-React
 package.json is a configuration for NPM
 there are two type of dependencies, dev(generally required for developing environment) and prod/normal(Also used in production).
+cart(^) vs Tilde(~): The caret symbol (^) is used to specify a version range that allows for updates to the minor version and patch version.
+The tilde symbol (~) is used to specify a version range that allows for updates to the patch version.
+package.lock: keep track of the version of the packages installed. maintains a hash to make sure whaterver is in my local is deployed on the server too.
+Node Module: all the modules we installed via npm i
+Transitive dependencies: we installed parcel, but got additional node modules as parcel need them
