0872cf0c9ee99ce32a10f785fd115a4dc43b7a0f
monika pant <monikapant@mukeshs-MacBook-Pro.local>
2025-02-04 23:41:43 +0530

    Episode4: design swiggy app

--- App.js
+++ App.js
@@ -1,18 +1,64 @@
-        // const heading = React.createElement('h1', {id:"heading1", xyz:"testing"}, 'my react element');
-        // const root = ReactDOM.createRoot(document.getElementById("root"));
-        // root.render(heading);   
-        import React from "react";
-        import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-        const parent = React.createElement('div', {id:"parent"}, [
-                React.createElement('div', {id:"sibling1"}, [
-                        React.createElement('h1', {id:"heading1"}, 'child1 h1'),
-                        React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
-                ]),
-                React.createElement('div', {id:"sibling2"}, [
-                        React.createElement('h1', {id:"heading1"}, 'child1 h1'),
-                        React.createElement('h1', {id:"heading2"}, 'child2 and sibling h2')
-                ])
-        ]);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-                root.render(parent);   
+/****
+ * Header
+ *      - Logo
+ *      - Nav items
+ * Body
+ *      - Search
+ *      - RestaurantContsiner
+ *      - RestaurantCard
+ * Footer
+ *      - copyright
+ *      - Links
+ *      - Address
+ *      - Contact
+ */
+import "./index.css";
+const Header = () => {
+  return (
+    <>
+      <div className="HeaderContainer">
+        <div className="logo">
+          <img src="https://img.freepik.com/free-vector/delivery-logo-template_23-2147880262.jpg"></img>
+        </div>
+        <div className="NavItems">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+const AppBody = () => {
+  return (
+    <>
+      <div className="CardContainer"></div>
+    </>
+  );
+};
+const Footer = () => {
+  return (
+    <>
+      <ul>
+        <li>copyright</li>
+      </ul>
+    </>
+  );
+};
+const AppLayoutComponent = () => {
+  return (
+    <>
+      <Header />
+      <AppBody />
+      <Footer />
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayoutComponent />);
